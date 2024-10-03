12c246ab8877806cee6f2c7dbe4ef08273a29332
Theo Le <theo.le.goldenowl@gmail.com>
2024-10-03 17:00:03 +0700

    Solution Isp

--- Solid/4.Lsp/main.ts
+++ Solid/4.Lsp/main.ts
@@ -8,29 +8,35 @@
  * Happy coding! 🚀
  */
 
-// interface Machine {
-//     print(document: string): void;
-//     scan(document: string): void;
-//     fax(document: string): void;
-// }
+import { Fax } from "./interfaces/Fax";
+import { Printer } from "./interfaces/Printer";
+import { Scanner } from "./interfaces/Scanner";
 
-// class OldFashionedPrinter implements Machine {
-//     print(document: string): void {
-//         console.log(`Printing document: ${document}`);
-//     }
+class OldFashionedPrinter implements Printer {
+    print(document: string): void {
+        console.log(`OldFashionedPrinter is printing document: ${document}`);
+    }
+}
 
-//     scan(document: string): void {
-//         throw new Error("Scan not supported");
-//     }
+class MultiFunctionPrinter implements Printer, Scanner, Fax { 
+    print(document: string): void {
+        console.log(`MultiFunctionPrinter is printing document: ${document}`);
+    }
 
-//     fax(document: string): void {
-//         throw new Error("Fax not supported");
-//     }
-// }
+    scan(document: string): void {
+        console.log(`MultiFunctionPrinter is scanning document: ${document}`);
+    }
 
-// const printer = new OldFashionedPrinter();
-// printer.print("Document 1");
+    fax(document: string): void {
+        console.log(`MultiFunctionPrinter is faxing document: ${document}`);
+    }
+}
 
-// printer.scan("Document 2"); // Lỗi: Scan not supported
-// printer.fax("Document 3");  // Lỗi: Fax not supported
+const oldPrinter = new OldFashionedPrinter();
+oldPrinter.print("Document 1");
+
+const newPrinter = new MultiFunctionPrinter(); 
+newPrinter.print("Document 2");
+newPrinter.scan("Document 3");
+newPrinter.fax("Document 4"); 
 
