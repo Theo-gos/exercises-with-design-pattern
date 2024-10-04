3f08dc290ecc5daa1f35bfe7247e4ab99350d235
Theo Le <theo.le.goldenowl@gmail.com>
2024-10-04 11:00:06 +0700

    Added tesT

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,10 @@ module.exports = {
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     collectCoverage: true,
     collectCoverageFrom: [
-        'Solid/**/*.ts',
-        '!Solid/**/*.d.ts',
-        '!Solid/**/*/main.ts'
+        'Solid/5.Dip/**/*.ts',
+        '!Solid/5.Dip/**/*.d.ts',
+        '!Solid/5.Dip/main.ts',
+        '!Solid/5.Dip/interfaces/**/*.ts',
     ],
     coverageDirectory: 'coverage',
     coverageReporters: ['json', 'lcov', 'text', 'clover'],
